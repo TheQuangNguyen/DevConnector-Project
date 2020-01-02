02e66765b42a8ab48de459e85372129cd6fe3937
Quang Nguyen <quangn2595@gmail.com>
2020-01-02 14:51:04 -0800

    establish route to register new user with name, email, and pasword to mongodb

---
+++ routes/api/users.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+const User = require("../../models/User");
+
+// @route     GET api/users
+// @desc      Test route
+// @access    Public
+router.post(
+  "/",
+  // validate the input and report any errors before creating the user
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    // if there are errors, send a 400 and error message back to front end
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // see if the user exists
+      let user = await User.findOne({ email });
+
+      // if the user already exist in database, then return 400 and return error message
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exist" }] });
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        // default size
+        s: "200",
+        // rating
+        r: "pg",
+        // default image
+        d: "mm"
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt password using bCrypt
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonwebtoken to front end for user to log in right away after registering
+
+      res.send("User registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
+  }
+);
+
+module.exports = router;
