5d8c662bc3e79ed93942d6b15e8a5d0cefe64230
Quang Nguyen <quangn2595@gmail.com>
2020-01-06 15:45:34 -0800

    added logout functionality and make navbar links to change depending on if the user is logged in or not

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,9 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -92,4 +94,9 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+// Logout user / Clear Profile
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT_SUCCESS });
 };
